901cfb28263d3a75a7ff216b2d6b3b5c5ec89010
eliteGrass <15688179817@163.com>
2024-12-05 22:41:52 +0800

    mq学习-springboot进行集成消息

--- mq/src/main/java/com/liteGrass/mq/demo/normal/SpringBootNormalProduct.java
+++ mq/src/main/java/com/liteGrass/mq/demo/normal/SpringBootNormalProduct.java
@@ -1,7 +1,15 @@
 package com.liteGrass.mq.demo.normal;
 
 
+import cn.hutool.core.date.DateUtil;
+import org.apache.rocketmq.client.apis.producer.SendReceipt;
+import org.apache.rocketmq.client.core.RocketMQClientTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * @Description 普通消息
@@ -11,8 +19,23 @@ import org.springframework.stereotype.Service;
 @Service
 public class SpringBootNormalProduct {
 
+    @Value("${rocketmq.normal.topic.flowTopic}")
+    private String normalTopic;
 
-    public boolean sendNormal() {
+    @Resource
+    private RocketMQClientTemplate rocketMQClientTemplate;
+
+    /**
+     *
+     * 异步发送消息到mq
+     * @return
+     */
+    public boolean asyncSendNormalMessage() {
+        CompletableFuture<SendReceipt> sendCompletableFuture = rocketMQClientTemplate.asyncSendNormalMessage(normalTopic, "test_message" + DateUtil.now(), new CompletableFuture<>());
+        // 如果消息发送失败，进行日志记录功能，重新进行发送
+        sendCompletableFuture.whenCompleteAsync((res, ex) -> {
+
+        })
         // 处理业务逻辑
         return true;
     }
