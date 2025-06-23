482eb89ada1b9b0a247d76fef4d7f1fcb560ac66
eliteGrass <15688179817@163.com>
2025-06-23 21:20:20 +0800

    netty相关方法提交

---
+++ learnNetty/baseNetty/src/test/java/com/liteGrass/learnNetty/baseNetty/baseNIO/BaseLearn1.java
@@ -0,0 +1,94 @@
+package com.liteGrass.learnNetty.baseNetty.baseNIO;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+/**
+ * @ClassName BaseLearn1
+ * @Company Huahui Information Technology Co., LTD.
+ * @Author elitegrass
+ * @Date 2025/6/17 00:05
+ * @Description
+ */
+public class BaseLearn1 {
+
+    @Test
+    public void testMethod1() throws IOException {
+        // 获取channel
+        FileChannel channel = new FileInputStream("/Users/elitegrass/Documents/ideaProjects/G-JDAT/learnNetty/baseNetty/src/main/resources/test.txt").getChannel();
+        // 获取buffer
+        ByteBuffer buffer = ByteBuffer.allocate(10);
+        channel.read(buffer);
+        // 切换为读模式
+        buffer.flip();
+        while (buffer.hasRemaining()) {
+            System.out.print((char) buffer.get());
+        }
+        buffer.clear();
+    }
+
+    @Test
+    public void testMethod2() throws IOException {
+        // 获取channel
+        FileChannel channel = new FileInputStream("/Users/elitegrass/Documents/ideaProjects/G-JDAT/learnNetty/baseNetty/src/main/resources/test.txt").getChannel();
+        ByteBuffer buffer = ByteBuffer.allocate(10);
+        while (true) {
+            // 获取buffer
+            if (channel.read(buffer) == -1) {
+                break;
+            }
+            // 切换为读模式
+            buffer.flip();
+            while (buffer.hasRemaining()) {
+                System.out.print((char) buffer.get());
+            }
+            buffer.clear();
+        }
+    }
+
+
+    @Test
+    public void testMethod3() throws FileNotFoundException {
+        try(FileChannel channel = new RandomAccessFile("/Users/elitegrass/Documents/ideaProjects/G-JDAT/learnNetty/baseNetty/src/main/resources/test.txt", "rw").getChannel()) {
+            ByteBuffer buffer = ByteBuffer.allocate(10);
+            while (true) {
+                // 获取buffer
+                if (channel.read(buffer) == -1) {
+                    break;
+                }
+                // 切换为读模式
+                buffer.flip();
+                while (buffer.hasRemaining()) {
+                    System.out.print((char) buffer.get());
+                }
+                buffer.clear();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+    @Test
+    public void testMethod4() throws IOException {
+        try(FileChannel channel = FileChannel.open(Paths.get("/Users/elitegrass/Documents/ideaProjects/G-JDAT/learnNetty/baseNetty/src/main/resources/test.txt"), StandardOpenOption.READ)) {
+
+        } catch (IOException) {
+
+        }
+
+    }
+
+}
